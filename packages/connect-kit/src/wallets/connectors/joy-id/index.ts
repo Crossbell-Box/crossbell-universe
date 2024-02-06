871b218ceb36ec7792209aa2da743d3685d194e4
runjuu <hi@runjuu.com>
2024-02-05 17:37:01 -0800

    fix(connect-kit/connector): JoyId lost connection state after refreshing the page

--- packages/connect-kit/src/wallets/connectors/joy-id/index.ts
+++ packages/connect-kit/src/wallets/connectors/joy-id/index.ts
@@ -5,15 +5,24 @@ export type JoyIdConnectorOptions = ConstructorParameters<
 >[0];
 
 export class JoyIdConnector extends BaseConnector {
+	async isAuthorized() {
+		return !!(await this.getAccount());
+	}
+
 	async getProvider(config?: { chainId?: number }) {
 		const provider = await super.getProvider(config);
+		const account = await this.getAccount();
 
 		if (!("on" in provider)) {
 			Object.defineProperty(provider, "on", {
-				value: (key: string) => {
+				value: (key: string, callback: (...params: any[]) => void) => {
 					console.warn(
 						`JoyIdConnector: \`provider.on("${key}", callback)\` not implemented`,
 					);
+
+					if (key === "accountsChanged" && account) {
+						callback([account]);
+					}
 				},
 			});
 		}
